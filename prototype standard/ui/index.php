682c9da2bede8f8e222e4ba6b4954a8095e03fef
imane-aj <ajroudi.im@gmail.com>
2022-10-12 04:59:56 -0700

    css

--- prototype standard/ui/index.php
+++ prototype standard/ui/index.php
@@ -17,24 +17,59 @@ require_once("../businessLayer/promoManagement.php");
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gestion des promo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+   <style>
       .hero{
         margin-top: 3em;
-        text-align:center;
+        text-align: center;
+        border: 1px solid #e5e5e5;
+        padding: 3px 33px;
       }
       .btn{
-        margin-bottom: 3em;
+        margin-bottom: 2em;
         display: flex;
         justify-content: space-between;
       }
-      .btn button{
+      /* .btn button{
         border: unset;
         border-radius: 7px;
         padding: 11px 36px;
-      }
+      } */
       .btn button:first-child{
         background: #52c5d647;
+      }
+      table {
+          border-collapse: unset;
+      }
+      .fa-trash-can{
+        color:red;
+      }
+      .fa-pen-to-square{
+        color: green;
+        margin-right: 2em;
+      }
+      form input{
+        position: relative;
+      }
+      form button{
+        position: absolute;
+        margin-left: 10em;
+        border: unset;
+        background:unset;
+      }
+      form button i{
+        color: #008000;
+      }
+      .btn a {
+        color: #6a6a6a;
+        text-decoration:none;
+      }
+      .btn a i{
+        border-radius: 50%;
+        border: 1px solid;
+        padding: 4px;
+        color: blue;
       }
     </style>
   </head>
@@ -44,28 +79,32 @@ require_once("../businessLayer/promoManagement.php");
         <div class="col-md-6">
           <div class="hero">
             <div class="btn">
-              <button>Ajouter promotion</button> 
-              <button>Chercher Promotion</button> 
+              <a href="ajout.php"><i class="fa-regular fa-plus"></i> Promotion</a>
+              <form class="d-flex" role="search">
+                <input type="search" placeholder="Search" aria-label="Search">
+                <button type="submit"><i class="fa-brands fa-searchengin"></i></button>
+              </form> 
             </div>
             <div class="show">
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Pormotion</th>
-      <th scope="col">Update</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach($data as $value){ ?>
-    <tr>
-      <th scope="row"><?php echo $value->getPromo() ?></th>
-      <td><a class="btn btn-success" href="update.php?id=<?php echo $value->getId() ?>">Update</a></td>
-      <td><a class="btn btn-danger" href="index.php?id=<?php echo $value->getId() ?>" onclick="return confirm('Are you sure?')">Delete</a></td>
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>
+            <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">Pormotion</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach($data as $value){ ?>
+                  <tr>
+                    <td><?php echo $value->getPromo() ?></td>
+                    <td>
+                      <a href="update.php?id=<?php echo $value->getId() ?>"><i class="fa-regular fa-pen-to-square"></i></a>
+                      <a href="index.php?id=<?php echo $value->getId() ?>" onclick="return confirm('Are you sure?')"><i class="fa-regular fa-trash-can"></i></a>
+                    </td>
+                  </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
